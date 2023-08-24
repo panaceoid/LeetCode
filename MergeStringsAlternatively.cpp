9c4c7ab54b0883dce4722886acbf03bb49020331
Kari <72104122+panaceoid@users.noreply.github.com>
2023-08-24 07:07:53 +0100

    Add files via upload
    Added two of my LeetCode 75 solutions

---
+++ MergeStringsAlternatively.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Solution {
+public:
+    char word1="word1";
+    char word2="word2";
+    char word3;
+    int mergedstringsize;
+
+    string mergeAlternately(string word1, string word2) {   
+        char charArray1[word1.length()]; 
+    
+        for (size_t i = 0; i < word1.length(); ++i) {
+        charArray1[i] = word1[i]; // Assign each         character to the array
+
+        char charArray2[word2.length()]; 
+    
+        for (size_t i = 0; i < word2.length(); ++i) {
+        charArray2[i] = word2[i]; // Assign each         character to the array
+
+    int mergedstringsize = word2.length() + word1.length();
+
+        for (size_t i=0; i < word2.length() ; I++)
+        {
+            for (a=0; a < word1.length(); a++)
+            {
+                charArray1[a].
+                return 
+            }
+            
+        }
+
+    }
+    }
+};
