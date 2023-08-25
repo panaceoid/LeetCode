21a3d39252d7ae0e18888de2eb23e4ef35a6aa0c
melkkari <72104122+melkkari@users.noreply.github.com>
2023-08-25 03:27:03 +0100

    Finished GCD From Strings problem. No real update to MergeStrings problem, just tweaks.

--- LeetCode 75/MergeStringsAlternatively.cpp
+++ LeetCode 75/MergeStringsAlternatively.cpp
@@ -12,12 +12,12 @@ public:
     
         for (size_t i = 0; i < word1.length(); ++i) {
         charArray1[i] = word1[i]; // Assign each         character to the array
-
+        }
         char charArray2[word2.length()]; 
     
         for (size_t i = 0; i < word2.length(); ++i) {
         charArray2[i] = word2[i]; // Assign each         character to the array
-
+        }
     int mergedstringsize = word2.length() + word1.length();
 
         for (size_t i=0; i < word2.length() ; I++)
@@ -25,11 +25,10 @@ public:
             for (a=0; a < word1.length(); a++)
             {
                 charArray1[a].
-                return 
+                
             }
             
         }
 
-    }
     }
 };
