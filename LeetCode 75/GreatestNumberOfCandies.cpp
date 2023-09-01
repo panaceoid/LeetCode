2cd66e6ad345e81e9945dbc6bcd02e7be8655a84
elkkarim <elkkarim@stberns.bristol.sch.uk>
2023-09-01 15:43:01 +0100

    just started a new problem

---
+++ LeetCode 75/GreatestNumberOfCandies.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+       //just starting this problem out.
+       i=0;
+       forcandies[i] 
+    }
+};
